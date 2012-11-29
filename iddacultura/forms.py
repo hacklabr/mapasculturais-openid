a8fb56d7477e8079a90e3ce433948f4914416caa
Rodrigo Primo <rodrigo@hacklab.com.br>
2012-11-29 16:20:50 -0200

    permite que usuario altere seu email e nome

--- iddacultura/forms.py
+++ iddacultura/forms.py
@@ -1,5 +1,51 @@
+#coding: utf-8
+
 from registration.forms import RegistrationForm
 from django.contrib.localflavor.br.forms import BRCPFField
+from django.forms import ModelForm
+from django import forms
+from iddacultura.models import User, UserProfile
 
 class UserRegistrationForm(RegistrationForm):
+    """
+    Extende o formulário de registro de usuário para
+    adicionar o campo CPF
+    """
+    
     cpf = BRCPFField()
+    
+
+class UserProfileForm(ModelForm):
+    """
+    Extende o formulário de edição do perfil do usuário
+    para adicionar os campos que são padrão do Django.
+    """
+    
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+        try:
+            self.fields['email'].initial = self.instance.user.email
+            self.fields['first_name'].initial = self.instance.user.first_name
+            self.fields['last_name'].initial = self.instance.user.last_name
+        except User.DoesNotExist:
+            pass
+ 
+    first_name = forms.CharField(label="Nome",help_text='')
+    last_name = forms.CharField(label="Sobrenome",help_text='')
+    email = forms.EmailField(label="E-mail",help_text='')
+ 
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)        
+ 
+    def save(self, *args, **kwargs):
+        """
+        Update the primary email address on the related User object as well.
+        """
+        u = self.instance.user
+        u.email = self.cleaned_data['email']
+        u.first_name = self.cleaned_data['first_name']
+        u.last_name = self.cleaned_data['last_name']
+        u.save()
+        profile = super(UserProfileForm, self).save(*args,**kwargs)
+        return profile
--- iddacultura/urls.py
+++ iddacultura/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 from registration.views import register
-from forms import UserRegistrationForm
+from forms import UserRegistrationForm, UserProfileForm
 from iddacultura.views import user_profile
 
 from django.contrib import admin
@@ -15,6 +15,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/register/$', register, {'backend': 'iddacultura.regbackend.RegBackend', 'form_class': UserRegistrationForm}, name='registration_register'),
     url(r'^accounts/', include('registration.backends.default.urls')),
+    url(r'^profiles/edit', 'profiles.views.edit_profile', {'form_class': UserProfileForm}),
     url(r'^profiles/', include('profiles.urls')),
     url(r'^provider/', include('iddacultura.provider.urls')),
 )
