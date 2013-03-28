3697c94801695b00dd1dd96dc394244d00736839
Rodrigo Primo <rodrigo@hacklab.com.br>
2013-03-28 10:10:34 -0300

    limpa os campos abaixo na hierarquia quando o usuario muda um dos campos da sua ocupacao (refs #1305)

--- iddacultura/static/js/edit_profile.js
+++ iddacultura/static/js/edit_profile.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
+    // quando o usuário muda um dos campos da sua ocupação atualiza a lista de opções do campo seguinte
+    // e desabilita os demais campos que estão abaixo na hierarquia
     $('.user_occupation').change(function() {
         // pega o select seguinte que é controlado pelo que foi alterado
-        var childSelect = $(this).parent().parent().nextAll().find('.user_occupation').first()
+        var childSelect = $(this).parent().parent().nextAll().find('.user_occupation').first();
         
         if (childSelect.length > 0) {
             $('body').css('cursor', 'wait');
@@ -11,6 +13,15 @@ $(document).ready(function() {
                     childSelect.append($("<option></option>").attr("value", key).text(val));
                 })
                 childSelect.removeAttr('disabled');
+                
+                // limpa e desabilita todos os selects que estão abaixo do childSelect na hierarquia
+                childSelect.parent().parent().nextAll().find('.user_occupation').each(function(key, e) {
+                    if (!$(e).attr('disabled')) {
+                        $(e).children().remove();
+                        $(e).append($("<option></option>").text('---------'));
+                        $(e).attr('disabled', 'disabled');
+                    }
+                });
                 $('body').css('cursor', 'auto');
             });
         }
