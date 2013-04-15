8fbb203f08ad670c6546402483ad4b5fc2497241
Rodrigo Primo <rodrigo@hacklab.com.br>
2013-04-15 17:16:40 -0300

    mantem o parametro next apos mudancas no html (refs #1400)

--- iddacultura/templatetags/login_form.py
+++ iddacultura/templatetags/login_form.py
@@ -2,6 +2,11 @@ from django import template
 from django.contrib.auth.forms import AuthenticationForm
 register = template.Library()
 
-@register.inclusion_tag('iddacultura/login_form.html')
-def login_form():
-    return { 'form': AuthenticationForm }
+@register.inclusion_tag('iddacultura/login_form.html', takes_context = True)
+def login_form(context):
+    data = { 'form': AuthenticationForm }
+    
+    if context.has_key('next'):
+        data['next'] = context['next']
+        
+    return data
