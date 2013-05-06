dba33fac93a08cfb5d1e5b6cd543ec301f5c76c2
Rafael Gutierri <gutierri@hacklab.com.br>
2013-05-06 15:14:54 -0300

    Ajustes no js, e ordem das selects no chrome

--- iddacultura/static/js/edit_profile.js
+++ iddacultura/static/js/edit_profile.js
@@ -4,16 +4,21 @@ $(document).ready(function() {
     $('.user_occupation').change(function() {
         // pega o select seguinte que é controlado pelo que foi alterado
         var childSelect = $(this).parent().parent().nextAll().find('.user_occupation').first();
-        
         if (childSelect.length > 0) {
             $('body').css('cursor', 'wait');
             $.getJSON('/occupations/?parent=' + $(this).val(), function(data) {
                 childSelect.children().remove();
+
+                    $('#id_user_occupation_secondary').prepend("<option>--Selecione--</option>");
+                    $('#id_user_occupation_tertiary').prepend("<option>--Selecione--</option>");
+                    $('#id_user_occupation_quartenary').prepend("<option>--Selecione--</option>");
+                    $('#id_user_occupation_quinary').prepend("<option>--Selecione--</option>");
+
                 $.each(data, function(key, val) {
                     childSelect.append($("<option></option>").attr("value", key).text(val));
                 })
                 childSelect.removeAttr('disabled');
-                
+
                 // limpa e desabilita todos os selects que estão abaixo do childSelect na hierarquia
                 childSelect.parent().parent().nextAll().find('.user_occupation').each(function(key, e) {
                     if (!$(e).attr('disabled')) {
