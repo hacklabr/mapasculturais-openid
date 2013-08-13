81eba25e94752e10cf0b25628e5fce652ec34bcb
Rodrigo Primo <rodrigo@hacklab.com.br>
2013-08-13 16:51:20 -0300

    exibe qual o item do menu esta ativo (fixes #1660 @90m)

--- iddacultura/templatetags/sidebar.py
+++ iddacultura/templatetags/sidebar.py
@@ -1,11 +1,22 @@
 from django import template
+from django.core.urlresolvers import reverse
 register = template.Library()
 
-@register.inclusion_tag('iddacultura/sidebar.html', takes_context = True)
-def sidebar(context):
+
+@register.inclusion_tag('iddacultura/sidebar.html', takes_context=True)
+def sidebar(context, path):
     data = {}
-    
-    if context.has_key('user'):
+
+    data['path'] = path
+    data['homepage'] = reverse('homepage')
+    data['viewprofile'] = reverse(
+        'profiles.views.profile_detail',
+        args=[context['user']]
+    )
+    data['editprofile'] = reverse('profiles.views.edit_profile')
+    data['chpassword'] = reverse('django.contrib.auth.views.password_change')
+
+    if 'user' in context:
         data['user'] = context['user']
-        
+
     return data
