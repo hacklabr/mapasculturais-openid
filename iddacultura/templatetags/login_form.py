7c8c301836c0144a0dfc9a1265b8a9d906c9e743
Rodrigo Primo <rodrigo@hacklab.com.br>
2013-09-12 16:13:32 -0300

    pep 8

--- iddacultura/templatetags/login_form.py
+++ iddacultura/templatetags/login_form.py
@@ -2,16 +2,17 @@ from django import template
 from django.contrib.auth.forms import AuthenticationForm
 register = template.Library()
 
-@register.inclusion_tag('iddacultura/login_form.html', takes_context = True)
+
+@register.inclusion_tag('iddacultura/login_form.html', takes_context=True)
 def login_form(context):
     data = {}
-    
-    if context.has_key('form'):
+
+    if 'form' in context:
         data['form'] = context['form']
     else:
-        data['form'] = AuthenticationForm 
-    
-    if context.has_key('next'):
+        data['form'] = AuthenticationForm
+
+    if 'next' in context:
         data['next'] = context['next']
-        
+
     return data
