174d49e2797e5eecae911a1574ab1627bfdcc5d9
Rodrigo Primo <rodrigo@hacklab.com.br>
2013-11-26 17:14:30 -0200

    avisa usuario quando as chaves do recaptcha nao estiverem definidas

--- iddacultura/forms.py
+++ iddacultura/forms.py
@@ -7,6 +7,8 @@ from django.forms import ModelForm, ValidationError
 from django import forms
 from iddacultura.models import User, UserProfile
 from captcha.fields import ReCaptchaField
+import settings
+from django.core.exceptions import ImproperlyConfigured
 
 
 class UserRegistrationForm(RegistrationFormUniqueEmail):
@@ -15,9 +17,15 @@ class UserRegistrationForm(RegistrationFormUniqueEmail):
     mais campos.
     """
 
+    def __init__(self, *args, **kwargs):
+        super(UserRegistrationForm, self).__init__(*args, **kwargs)
+
+        if not settings.RECAPTCHA_PUBLIC_KEY or not settings.RECAPTCHA_PRIVATE_KEY:
+            raise ImproperlyConfigured('É necessário definir as chaves do ReCaptcha no settings_local.py')
+
     first_name = forms.CharField(label="Nome", help_text='',)
     last_name = forms.CharField(label="Sobrenome", help_text='')
-#    captcha = ReCaptchaField(attrs={'theme': 'white'})
+    captcha = ReCaptchaField(attrs={'theme': 'white'})
 
 
 class UserProfilePublicForm(ModelForm):
