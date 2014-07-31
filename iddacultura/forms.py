07172e3048a11ec53eb6ef7e535e5852026e42d4
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2014-07-31 17:54:12 -0300

    melhora visualização de erros no form de cadastro

--- iddacultura/forms.py
+++ iddacultura/forms.py
@@ -21,6 +21,9 @@ class UserRegistrationForm(RegistrationFormUniqueEmail):
         if not settings.RECAPTCHA_PUBLIC_KEY or not settings.RECAPTCHA_PRIVATE_KEY:
             raise ImproperlyConfigured('É necessário definir as chaves do ReCaptcha no settings_local.py')
 
+    username = forms.RegexField(label="Usuário", max_length=75,regex=r"^[\w.@+-]+$",
+                                help_text="Utilize apenas letras e número sem espaços")
+
     first_name = forms.CharField(label="Nome", help_text='',)
     last_name = forms.CharField(label="Sobrenome", help_text='')
     captcha = ReCaptchaField(attrs={'theme': 'white'})
