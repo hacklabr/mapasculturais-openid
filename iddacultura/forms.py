3e8bbff52cc0827ed970ec2a3cc1f786c6d785b4
Rafael Monico <monico@sanjose>
2014-10-24 16:36:10 -0200

    home template inserting registration template

--- iddacultura/forms.py
+++ iddacultura/forms.py
@@ -1,32 +1,32 @@
 # -*- coding: utf-8 -*-
 
-# from registration.forms import RegistrationFormUniqueEmail
+from registration.forms import RegistrationFormUniqueEmail
 # from django.forms import ModelForm
-# from django import forms
+from django import forms
 # from iddacultura.models import User, UserProfile
-# from captcha.fields import ReCaptchaField
-# import settings
-# from django.core.exceptions import ImproperlyConfigured
+from captcha.fields import ReCaptchaField
+import settings
+from django.core.exceptions import ImproperlyConfigured
 
 
-# class UserRegistrationForm(RegistrationFormUniqueEmail):
-#     """
-#     Extende o formulário de registro de usuário para
-#     mais campos.
-#     """
+class UserRegistrationForm(RegistrationFormUniqueEmail):
+    """
+    Extende o formulário de registro de usuário para
+    mais campos.
+    """
 
-#     def __init__(self, *args, **kwargs):
-#         super(UserRegistrationForm, self).__init__(*args, **kwargs)
+    username = forms.RegexField(label="Usuário", max_length=75, regex=r"^[\w.@+-]+$",
+                                help_text="Utilize apenas letras e número sem espaços")
 
-#         if not settings.RECAPTCHA_PUBLIC_KEY or not settings.RECAPTCHA_PRIVATE_KEY:
-#             raise ImproperlyConfigured('É necessário definir as chaves do ReCaptcha no settings_local.py')
+    first_name = forms.CharField(label="Nome", help_text='',)
+    last_name = forms.CharField(label="Sobrenome", help_text='')
+    captcha = ReCaptchaField(attrs={'theme': 'white'})
 
-#     username = forms.RegexField(label="Usuário", max_length=75,regex=r"^[\w.@+-]+$",
-#                                 help_text="Utilize apenas letras e número sem espaços")
+    def __init__(self, *args, **kwargs):
+        super(UserRegistrationForm, self).__init__(*args, **kwargs)
 
-#     first_name = forms.CharField(label="Nome", help_text='',)
-#     last_name = forms.CharField(label="Sobrenome", help_text='')
-#     captcha = ReCaptchaField(attrs={'theme': 'white'})
+        if not (settings.RECAPTCHA_PUBLIC_KEY and settings.RECAPTCHA_PRIVATE_KEY):
+            raise ImproperlyConfigured('É necessário definir as chaves do ReCaptcha no settings_local.py')
 
 
 # class UserPublicForm(ModelForm):
