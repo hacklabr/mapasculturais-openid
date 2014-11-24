bff90470465ef14b70fd5628fa6372266df2b80e
Leonardo Santagada <santagada@gmail.com>
2014-11-24 20:03:40 -0200

    update theme pieces

--- iddacultura/context_processors.py
+++ iddacultura/context_processors.py
@@ -1,7 +1,10 @@
-
+# coding: utf-8
 
 def sitedata(request):
     return {
         'return_url': 'http://www.slashdot.org/',
-        'site_name': 'asfasdf'
+        'site_name': 'asfasdf',
+        'nome_org': 'Prefeitura de São Paulo',
+        'brand_logo': '',
+        # 'brand_logo': 'img/logo-spcultura.png',
     }
