60964e8b23f5b6d3b7d955e4f070779d506feb92
Leonardo Santagada <santagada@gmail.com>
2014-11-24 20:28:12 -0200

    reorder values and generate default id

--- iddacultura/forms.py
+++ iddacultura/forms.py
@@ -2,6 +2,7 @@
 
 from django import forms
 from allauth.account.forms import SignupForm
+from allauth.utils import set_form_field_order
 from captcha.fields import ReCaptchaField
 
 
@@ -11,10 +12,28 @@ class OpenIDSignupForm(SignupForm):
     mais campos.
     """
 
-    username = forms.RegexField(label="Usuário", max_length=75, regex=r"^[\w.@+-]+$",
-                                help_text="Utilize apenas letras e número sem espaços")
-
     first_name = forms.CharField(label="Nome", help_text='',)
     last_name = forms.CharField(label="Sobrenome", help_text='')
-
     captcha = ReCaptchaField(attrs={'theme': 'white'})
+
+    def __init__(self, *args, **kwargs):
+        ret = super(OpenIDSignupForm, self).__init__(*args, **kwargs)
+
+        first_fields = ['first_name', 'last_name']
+        merged_field_order = list(self.fields.keys())
+
+        for field in reversed(first_fields):
+            merged_field_order.remove(field)
+            merged_field_order.insert(0, field)
+
+        set_form_field_order(self, merged_field_order)
+
+        return ret
+
+    def signup(self, request, user):
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.save()
+        oid = OpenID(user=user, default=True)
+        oid.save()
+        return super(OpenIDSignupForm, self).signup(request, user)
