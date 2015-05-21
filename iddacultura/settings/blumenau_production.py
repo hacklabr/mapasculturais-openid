96af83ec640717f91d738b6d188879ec45f48f86
Bruno Martin <bruno.martin@hacklab.com.br>
2015-05-21 19:27:11 -0300

    create blumenau settings file

---
+++ iddacultura/settings/blumenau_production.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+from .base import *
+
+DEBUG = False
+# TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'openid-staging',
+    }
+}
+
+STATIC_ROOT = join(SITE_ROOT, '../webfiles-staging-blumenal/static/')
